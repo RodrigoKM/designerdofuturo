5baa6bcfbbb63ba5445399b5c231592ec981f4b4
Kur4m4 <Kur4m4@users.noreply.github.com>
2017-07-24 13:49:44 -0300

    conexão com base remota

--- src/com/designerdofuturo/conexaoBanco/ConexaoMySQL.java
+++ src/com/designerdofuturo/conexaoBanco/ConexaoMySQL.java
@@ -1,88 +1,70 @@
 //Nome do nosso pacote //
-package com.designerdofuturo.conexaoBanco; 
-
+package com.designerdofuturo.conexaoBanco;
 
 //Classes necessárias para uso de Banco de dados // 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException; 
+import java.sql.SQLException;
 
 //Início da classe de conexão// 
-public class ConexaoMySQL { 
+public class ConexaoMySQL {
 	public static String status = "Não conectou...";
-	
-	//Método Construtor da Classe// 
-	public ConexaoMySQL() { } 
-	
-	//Método de Conexão// 
-	public static java.sql.Connection getConexaoMySQL() { 
-		Connection connection = null; //atributo do tipo Connection 
-		try { 
-			// Carregando o JDBC Driver padrão 
-			String driverName = "com.mysql.jdbc.Driver"; 
-			Class.forName(driverName); 
-			
-			
+
+	// Método Construtor da Classe//
+	public ConexaoMySQL() {
+	}
+
+	// Método de Conexão//
+	public static java.sql.Connection getConexaoMySQL() {
+		Connection connection = null; // atributo do tipo Connection
+		try {
+			// Carregando o JDBC Driver padrão
+			String driverName = "com.mysql.jdbc.Driver";
+			Class.forName(driverName);
+
 			// Configurando a nossa conexão com um banco de dados//
-			String serverName = "127.0.0.1"; //caminho do servidor do BD 
-			String mydatabase = "localdb"; //nome do seu banco de dados 
-			String url = "jdbc:mysql://127.0.0.1:49717/localdb"; 
-			String username = "azure"; //nome de um usuário de seu BD 
-			String password = "6#vWHD_$"; //sua senha de acesso 
+			String serverName = "sql10.freemysqlhosting.net"; // caminho do servidor do BD
+			String mydatabase = "sql10186627"; // nome do seu banco de dados
+			String port = "3306"; // nome do seu banco de dados
+			String url = "jdbc:mysql://".concat(serverName).concat(":").concat(port).concat("/").concat(mydatabase);
+			String username = "sql10186627"; // nome de um usuário de seu BD
+			String password = "xvUNzzXdzj"; // sua senha de acesso
 			connection = DriverManager.getConnection(url, username, password);
-				
-			//Testa sua conexão// 
-			if (connection != null) { 
-				status = ("STATUS--->Conectado com sucesso!"); }
-			else { 
-				status = ("STATUS--->Não foi possivel realizar conexão"); 
-				} 
+
+			// Testa sua conexão//
+			if (connection != null) {
+				status = ("STATUS--->Conectado com sucesso!");
+			} else {
+				status = ("STATUS--->Não foi possivel realizar conexão");
+			}
 			return connection;
-			
-			
-			
-			} 	
-	
-		catch (ClassNotFoundException e) { //Driver não encontrado 
-			System.out.println("O driver expecificado nao foi encontrado."); 
-			return null; 
-				} 
-		catch (SQLException e) { //Não conseguindo se conectar ao banco 
-			System.out.println("Nao foi possivel conectar ao Banco de Dados."); 
-			return null; 
-				} 
-	        } 
-	
-	
-	
-	
-	//Método que retorna o status da sua conexão// 
-	public static String statusConection() { 
-		return status; 
-		} 
-	
-	
-	
-	//Método que fecha sua conexão// 
-	public static boolean FecharConexao() { 
+		} catch (ClassNotFoundException e) { // Driver não encontrado
+			System.out.println("O driver expecificado nao foi encontrado.");
+			return null;
+		} catch (SQLException e) { // Não conseguindo se conectar ao banco
+			System.out.println("Nao foi possivel conectar ao Banco de Dados.");
+			return null;
+		}
+	}
+
+	// Método que retorna o status da sua conexão//
+	public static String statusConection() {
+		return status;
+	}
+
+	// Método que fecha sua conexão//
+	public static boolean FecharConexao() {
 		try {
 			ConexaoMySQL.getConexaoMySQL().close();
-			return true; 
-			} 
-		catch (SQLException e) { 
-			return false; 
-			} 
-		} 
-	
-	
-	
-	//Método que reinicia sua conexão// 
-	public static java.sql.Connection ReiniciarConexao() { 
-		FecharConexao(); 
-		return ConexaoMySQL.getConexaoMySQL(); 
-		} 
+			return true;
+		} catch (SQLException e) {
+			return false;
+		}
+	}
 
+	// Método que reinicia sua conexão//
+	public static java.sql.Connection ReiniciarConexao() {
+		FecharConexao();
+		return ConexaoMySQL.getConexaoMySQL();
+	}
 }
-	
-
-

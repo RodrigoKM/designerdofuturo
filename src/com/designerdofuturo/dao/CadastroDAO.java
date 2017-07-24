da646dab1bb15f86d9a2503e7bf58c8343bb2975
Kur4m4 <Kur4m4@users.noreply.github.com>
2017-07-24 13:58:31 -0300

    DAO atualizado

--- src/com/designerdofuturo/dao/CadastroDAO.java
+++ src/com/designerdofuturo/dao/CadastroDAO.java
@@ -3,46 +3,36 @@ package com.designerdofuturo.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
-import com.designerdofuturo.conexaoBanco.ConexaoMySQL;
 import com.designerdofuturo.bean.Cadastro;
+import com.designerdofuturo.conexaoBanco.ConexaoMySQL;
 
 public class CadastroDAO {
-	
-    private Connection connection;    
-    
-    public CadastroDAO(){    
-        this.connection = ConexaoMySQL.getConexaoMySQL();  
-    }    
-    
-    public void adicionaCadastro(Cadastro cadastro){ 
-    
-       
-        
-      
-    	
-    	
-    	String sql = "INSERT INTO lead(email, nome, ip, stack, data_hora) VALUES(?,?,?,?,(SELECT NOW()))";  
-    	
-        try {   
 
-            PreparedStatement stmt = connection.prepareStatement(sql);    
+	private Connection connection;
 
-            stmt.setString(1, cadastro.getEmail());    
-            stmt.setString(2, cadastro.getNomeCompleto());    
-            stmt.setString(3, cadastro.getIP());   
-            stmt.setString(4, cadastro.getArea());    
-              
+	public CadastroDAO() {
+		this.connection = ConexaoMySQL.getConexaoMySQL();
+	}
 
-            stmt.execute();    
-            stmt.close();    
+	public void adicionaCadastro(Cadastro cadastro) {
 
-        } catch (SQLException u) {    
-            throw new RuntimeException(u);    
-        }    
-        
-        
-    }
+		String sql = "INSERT INTO lead(email, nome, ip, stack, data_hora) VALUES(?,?,?,?,(SELECT NOW()))";
 
+		try {
+
+			PreparedStatement stmt = connection.prepareStatement(sql);
+
+			stmt.setString(1, cadastro.getEmail());
+			stmt.setString(2, cadastro.getNomeCompleto());
+			stmt.setString(3, cadastro.getIp());
+			stmt.setString(4, cadastro.getStack());
+
+			stmt.execute();
+			stmt.close();
+
+		} catch (SQLException u) {
+			throw new RuntimeException(u);
+		}
+	}
 }

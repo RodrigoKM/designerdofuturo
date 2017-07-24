321f6ac4cf0ee79b97fa8e35bbe316d5342de69e
RodrigoKM <rodrigokoythi@hotmail.com>
2017-07-24 19:02:03 -0300

    toast para dar feedback para o usuario

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -1,6 +1,28 @@
 (function () {
-    $(document).ready(function () {
-        $('select').material_select();
-        $('.parallax').parallax();
-    });
+    $('select').material_select();
+    $('.parallax').parallax();
+
+    var getUrlParameter = function getUrlParameter(sParam) {
+        var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : sParameterName[1];
+            }
+        }
+    };
+
+    var status = getUrlParameter('status');
+
+    if (status === '1') {
+        Materialize.toast('Cadastrado com Sucesso!', 4000, 'green');
+    }
+    else if(status === '2'){
+        Materialize.toast('Email já cadastrado!', 4000, 'red');
+    }
 }());

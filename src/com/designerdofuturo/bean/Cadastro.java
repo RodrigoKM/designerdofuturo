457d34006b2481680f1d046409864c9aba51acb4
Kur4m4 <Kur4m4@users.noreply.github.com>
2017-07-24 13:48:00 -0300

    bean do Cadastro atualizado

--- src/com/designerdofuturo/bean/Cadastro.java
+++ src/com/designerdofuturo/bean/Cadastro.java
@@ -3,43 +3,46 @@ package com.designerdofuturo.bean;
 public class Cadastro {
 	private String nomeCompleto;
 	private String email;
-	private String area;
+	private String stack;
 	private String ip;
-	
-	public Cadastro( String nomeCompleto, String email, String area, String ip) {
-		
+
+	public Cadastro(String nomeCompleto, String email, String stack, String ip) {
 		super();
 		this.nomeCompleto = nomeCompleto;
 		this.email = email;
-		this.area = area;
+		this.stack = stack;
 		this.ip = ip;
-		
 	}
-	
+
 	public String getNomeCompleto() {
 		return nomeCompleto;
 	}
+
 	public void setNomeCompleto(String nomeCompleto) {
 		this.nomeCompleto = nomeCompleto;
 	}
-	
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public String getArea() {
-		return area;
-	}
-	public void setArea(String area) {
-		this.area = area;
-	}
-	public String getIP() {
-		return area;
-	}
-	public void setIP(String IP) {
-		this.ip = IP;
+
+	public String getStack() {
+		return stack;
 	}
 
+	public void setStack(String stack) {
+		this.stack = stack;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
 }

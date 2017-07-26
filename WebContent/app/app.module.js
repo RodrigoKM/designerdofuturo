120f20ff8818f216fad258dd0a3dc8635b29b425
RodrigoKM <rodrigokoythi@hotmail.com>
2017-07-26 19:31:53 -0300

    scrolltop quando mudar de tela

--- WebContent/app/app.module.js
+++ WebContent/app/app.module.js
@@ -1,7 +1,12 @@
-(function() {
+(function () {
     'use strict';
 
     angular.module('app', [
         'ui.router'
-    ]);
+    ])
+        .run(function ($transitions) {
+            $transitions.onSuccess({}, function () {
+                document.body.scrollTop = document.documentElement.scrollTop = 0;
+            });
+        });
 })();
